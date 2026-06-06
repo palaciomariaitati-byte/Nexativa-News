d684c8792589b30acba33131906ee5395dc256f5
Javilo29 <javilo29.ajlg@gmail.com>
2026-06-06 10:24:57 -0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="text-4xl font-bold text-center">Nexativa News - Próximamente</h1>
+    </main>
+  );
+}
